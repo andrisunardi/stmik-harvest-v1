d04bea86aeaecab894ad9a5dd50b52ec548135df
Andri Sunardi <account@andrisunardi.com>
2022-05-23 21:45:20 +0700

    Validation Language

--- lang/en/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    "Copyright" => "Copyright",
-    "All Rights Reserved" => "All Rights Reserved",
-    "Created and Designed by" => "Created and Designed by",
-];
--- lang/id/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    "Copyright" => "Hak Cipta",
-    "All Rights Reserved" => "Seluruh Hak Cipta Dilindungi",
-    "Created and Designed by" => "Dibuat dan Dirancang oleh",
-];
