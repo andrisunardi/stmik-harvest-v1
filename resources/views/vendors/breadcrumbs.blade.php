0214b8eb0b3670b19ff0cce3d414c9fe9eb2d808
Andri Sunardi <account@andrisunardi.com>
2023-01-18 17:14:11 +0700

    Update Breadcrumbs

--- resources/views/vendors/breadcrumbs.blade.php
+++ resources/views/vendors/breadcrumbs.blade.php
@@ -1,17 +1,30 @@
-@unless ($breadcrumbs->isEmpty())
-    @foreach ($breadcrumbs as $breadcrumb)
-        @if (!is_null($breadcrumb->url) && !$loop->last)
-            <li class="breadcrumb-item active" aria-current="page">
-                <i class="{{ $breadcrumb->icon }}"></i>
-                <a draggable="false" href="{{ $breadcrumb->url }}">
+@if (Route::is("cms.*"))
+    @unless ($breadcrumbs->isEmpty())
+        @foreach ($breadcrumbs as $breadcrumb)
+            @if (!is_null($breadcrumb->url) && !$loop->last)
+                <li class="breadcrumb-item active" aria-current="page">
+                    <i class="{{ $breadcrumb->icon }}"></i>
+                    <a draggable="false" href="{{ $breadcrumb->url }}">
+                        {{ $breadcrumb->title }}
+                    </a>
+                </li>
+            @else
+                <li class="breadcrumb-item">
+                    <i class="{{ $breadcrumb->icon }}"></i>
                     {{ $breadcrumb->title }}
-                </a>
-            </li>
-        @else
-            <li class="breadcrumb-item">
-                <i class="{{ $breadcrumb->icon }}"></i>
-                {{ $breadcrumb->title }}
-            </li>
-        @endif
-    @endforeach
-@endunless
+                </li>
+            @endif
+        @endforeach
+    @endunless
+@else
+    @unless ($breadcrumbs->isEmpty())
+        @foreach ($breadcrumbs as $breadcrumb)
+            @if (!is_null($breadcrumb->url) && !$loop->last)
+                <a draggable="false" class="breadcrumb-item" href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a>
+                <span class="brd-separetor"><i class="icon ion-ios-arrow-right"></i></span>
+            @else
+                <span class="breadcrumb-item active text-white fw-bold">{{ $breadcrumb->title }}</span>
+            @endif
+        @endforeach
+    @endunless
+@endif
