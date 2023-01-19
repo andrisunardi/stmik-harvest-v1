f624dd4a3d3853cd59c4831a72d8368e2a2e3429
Andri Sunardi <account@andrisunardi.com>
2023-01-19 18:58:39 +0700

    Update Role Seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -10,19 +10,5 @@ class RoleSeeder extends Seeder
     public function run()
     {
         Role::create(['name' => 'Super User']);
-        Role::create(['name' => 'Board Of Directors']);
-        Role::create(['name' => 'Admin']);
-        Role::create(['name' => 'Finance']);
-        Role::create(['name' => 'Sales']);
-        Role::create(['name' => 'Development']);
-        Role::create(['name' => 'Staff']);
-        Role::create(['name' => 'Client']);
-        Role::create(['name' => 'Agent']);
-        Role::create(['name' => 'User']);
-        Role::create(['name' => 'Creator Forum']);
-        Role::create(['name' => 'Game Developer']);
-        Role::create(['name' => 'Job Poster']);
-        Role::create(['name' => 'Influencer']);
-        Role::create(['name' => 'Game Creator']);
     }
 }
