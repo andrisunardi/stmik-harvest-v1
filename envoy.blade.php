bdcac247c906a8843c08d5cd96bf6341cc2617cc
Andri Sunardi <account@andrisunardi.com>
2023-03-11 18:17:01 +0700

    Update envoy and env

--- envoy.blade.php
+++ envoy.blade.php
@@ -7,33 +7,34 @@
 @servers(['web' => env("DEPLOY_SERVER")])
 
 @task('deploy', ['on' => ['web']])
-    echo "Start Deploy"
+    echo Start Deploy
 
-    echo "DEPLOY SERVER :" {{ env('DEPLOY_SERVER') }}
-    echo "DEPLOY DIRECTORY :" {{ env('DEPLOY_DIRECTORY') }}
-    echo "DEPLOY BRANCH :" {{ env('DEPLOY_BRANCH') }}
-    echo "DEPLOY RENDER MAINTENANCE :" {{ env('DEPLOY_RENDER_MAINTENANCE') }}
+    echo DEPLOY SERVER : {{ env('DEPLOY_SERVER') }}
+    echo DEPLOY DIRECTORY : {{ env('DEPLOY_DIRECTORY') }}
+    echo DEPLOY BRANCH : {{ env('DEPLOY_BRANCH') }}
+    echo DEPLOY RENDER : {{ env('DEPLOY_RENDER') }}
+    echo DEPLOY SECRET : {{ env('DEPLOY_SECRET') }}
 
-    echo "cd directory" {{ $directory ?? env("DEPLOY_DIRECTORY") }}
+    echo cd directory {{ $directory ?? env("DEPLOY_DIRECTORY") }}
     cd {{ $directory ?? env("DEPLOY_DIRECTORY") }}
 
-    echo "php artisan down --render="{{ $maintenance ?? env("DEPLOY_RENDER_MAINTENANCE") }}
-    php artisan down --render={{ $maintenance ?? env("DEPLOY_RENDER_MAINTENANCE") }}
+    echo php artisan down --render={{ $render ?? env("DEPLOY_RENDER") }} --secret={{ $secret ?? env("DEPLOY_SECRET") }}
+    php artisan down --render={{ $render ?? env("DEPLOY_RENDER") }} --secret={{ $secret ?? env("DEPLOY_SECRET") }}
 
-    echo "Git Pull Origin" {{ $branch ?? env("DEPLOY_BRANCH") }}
+    echo Git Pull Origin {{ $branch ?? env("DEPLOY_BRANCH") }}
     git pull origin {{ $branch ?? env("DEPLOY_BRANCH") }}
 
-    echo "Composer Install"
+    echo Composer Install
     composer install
 
-    echo "php artisan migrate --force"
+    echo php artisan migrate --force
     php artisan migrate --force
 
-    echo "php artisan optimize:clear"
+    echo php artisan optimize:clear
     php artisan optimize:clear
 
-    echo "php artisan up"
+    echo php artisan up
     php artisan up
 
-    echo "End Deploy"
+    echo End Deploy
 @endtask
